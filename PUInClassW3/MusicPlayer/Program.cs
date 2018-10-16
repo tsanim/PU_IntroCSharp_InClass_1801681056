9373f17c794805e4f0b4a193b926247e1233104a
TsaniM <cancy@abv.bg>
2018-10-16 18:34:05 +0300

    My new music player

--- PUInClassW3/MusicPlayer/Program.cs
+++ PUInClassW3/MusicPlayer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,36 @@ namespace MusicPlayer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+           Console.WriteLine("Do you want play some music?");
+           string answer = Console.ReadLine();
+           
+          if(answer =="Yes")
+          {
+
+                Console.WriteLine("Playing music...!");
+
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\WorkFolderTsM\PU_IntroCSharp_InClass_1801681056\Music\Dim4ou_-_Official_Video-eAnF_HYKpQM.wav"))
+                {
+                    
+                    player.PlaySync();
+                }
+
+            }
+          else if (answer =="No")
+          {
+                Console.WriteLine("Goodbye!");
+          }
+          else
+          {
+                Console.WriteLine("Wrong input!");
+          }
+
+
+            
+            
+    
+
+                  
         }
     }
 }
